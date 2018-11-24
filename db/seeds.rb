28b5cdd8299058bb3a8334c4a0f82c318b130591
rosen777 <rosen.toshev@flatironschool.com>
2018-11-24 22:51:02 +0000

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+cp2 = Coupon.create(coupon_code: '123', store: 'Tesco')
